996bb4b066e3c5452db8bcfacdce9ea8e4226a7c
sameer <sameer189617@st.jmi.ac.in>
2021-08-31 14:44:48 +0530

    dsfdvghs

--- wiki/settings.py
+++ wiki/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%710m*zic)#0u((qugw#1@e^ty!c)9j04956v@ly(_86n$rg)h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['abc.com', '127.0.0.1']
+ALLOWED_HOSTS = ['bioscience.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -55,7 +55,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
-
 ROOT_URLCONF = 'wiki.urls'
 
 TEMPLATES = [
